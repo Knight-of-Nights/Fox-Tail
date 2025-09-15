e32f9ec6eac0f49538255d6fec2d84dfff356dea
Knight-of-Nights <martest@student.elms.edu>
2025-09-15 11:57:14 -0700

    Learning

---
+++ Lab 3 Currency Converter UI/script.js
@@ -0,0 +1,12 @@
+
+function Hello() { 
+  name = document.getElementById("username").value;
+  alert("Hello " + name + "!");
+}
+
+function Bye() {
+  name = document.getElementById("username").value;
+  alert("Bye " + name + "!");
+}
+
+
