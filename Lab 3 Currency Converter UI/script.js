9c399c2998b7d883e667aca7ac5e0c521c792af3
Knight-of-Nights <martest@student.elms.edu>
2025-09-17 11:57:04 -0700

    Refactor currency input fields and remove unused functions in index.html and script.js

--- Lab 3 Currency Converter UI/script.js
+++ Lab 3 Currency Converter UI/script.js
@@ -1,12 +1,10 @@
 
-function Hello() { 
-  name = document.getElementById("username").value;
-  alert("Hello " + name + "!");
+function Convert() {
+currency = document.getElementById("money1").value;
+  x = currency * 1376.02;
+  alert("That is " + x + " in South Korean Won.");
+
+currency = document.getElementById("money2").value;
+  x = currency / 1376.02;
+  alert("That is " + x + " in US Dollars.");
 }
-
-function Bye() {
-  name = document.getElementById("username").value;
-  alert("Bye " + name + "!");
-}
-
-
