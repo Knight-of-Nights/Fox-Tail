2f4d3682ad945556f34352d7edfd4d6ab33c8909
Knight-of-Nights <martest@student.elms.edu>
2025-11-05 12:24:43 -0500

    Pokemon added

--- Lab 7 React Pokedex/react-demo/src/App.js
+++ Lab 7 React Pokedex/react-demo/src/App.js
@@ -1,22 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 
+function Pokemon(props) {               
+        return <div class="Pokemon"> <h1> {props.name} </h1>
+        <h2> {props.type} </h2>
+        <img src={props.image}></img> 
+
+        </div>;
+}
+
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <Pokemon name="Pikachu" image="Pikachu.png" type="Electric"/>
+       <Pokemon name="Meowth" image="Meowth.png" type="Normal"/>
+       <Pokemon name="Snom" image="snom.png" type="Ice & Bug"/>
+       <Pokemon name="Bisharp" image="bisharp.png" type="Dark & Steel"/>
       </header>
     </div>
   );
