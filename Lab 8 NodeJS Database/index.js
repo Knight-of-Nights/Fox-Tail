767469ff7b5b8810c431d5e6f1024b60c641180a
Knight-of-Nights <martest@student.elms.edu>
2025-11-12 11:05:30 -0500

    Data

---
+++ Lab 8 NodeJS Database/index.js
@@ -0,0 +1,33 @@
+const express = require('express'); 
+const app = express();
+app.get('/', function(req, res){
+   const sql = 'SELECT * FROM students WHERE lastname = ?';
+const lastname = "Mouse"; // or get from form input
+conn.query(sql, [lastname], function (err, result) {
+  if (err) throw err;
+  console.log(result);
+ res.send(result);
+});
+
+
+});
+app.listen(8080);
+
+
+
+
+const mysql = require('mysql');
+require('dotenv').config();               
+const conn = mysql.createConnection({
+  host: "mysql1-p2.ezhostingserver.com",
+  database: "citdemo",
+user:"elms",
+password:"CIT4ever!"
+//  user: process.env.DB_USER,
+//  password: process.env.DB_PASSWORD
+});
+conn.connect((err) => {           // can move this into app.get and send
+  if (err) throw err;
+  console.log("Connected!");
+});
+
